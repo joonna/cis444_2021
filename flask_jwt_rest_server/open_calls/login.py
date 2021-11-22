adb383b91d7d284c48e867e95f538273aff6a995
Jonathan <43124873+joonna@users.noreply.github.com>
2021-11-21 20:54:44 -0800

    connecting to my database

--- flask_jwt_rest_server/open_calls/login.py
+++ flask_jwt_rest_server/open_calls/login.py
@@ -1,8 +1,12 @@
 from flask import request, g
 from flask_json import FlaskJSON, JsonError, json_response, as_json
 from tools.token_tools import create_token
-
+from db_con import get_db_instance, get_db
+import psycopg2    
 from tools.logging import logger
+import bcrypt
+
+global JWT
 
 def handle_request():
     logger.debug("Login Handle Request")
@@ -10,8 +14,29 @@ def handle_request():
 
     password_from_user_form = request.form['password']
     user = {
-            "sub" : request.form['firstname'] #sub is used by pyJwt as the owner of the token
+            "sub" : request.form['username'] #sub is used by pyJwt as the owner of the token
             }
+    cur = g.db.cursor()
+    cur.execute("select * from users where username = '" + request.form['username'] + "';")
+    dbcred = cur.fetchone()
+    cur.close()
+    #print(dbcred)
+    
+    if dbcred is None:
+        logger.debug("No User")
+        return json_response( data={"message": "Invalid user name: " + str(request.form['username'])}, status = 404)
+    else:
+        #print("else statement")
+        if bcrypt.checkpw(bytes(request.form['password'], "utf-8"), bytes(dbcred[2], "utf-8")) == True:
+            logger.debug("Successful Login, : " + str(request.form['username']))
+
+        else:
+            #print("Invalid password")
+
+            return json_response( data={"message": "Incorrect Password"}, status = 404)
+
+    return json_response( token = create_token(user) , authenticated = True)
+    
     if not user:
         return json_response(status_=401, message = 'Invalid credentials', authenticated =  False )
 
